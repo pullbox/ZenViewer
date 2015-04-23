82d0fe58f83d6db3a9b9cb95a7a656f05582d829
pullb0x <debicool98@gmail.com>
2015-04-22 23:48:23 -0400

    new API Access Object

---
+++ TicketView/src/net/bechtelus/standard/APIAccessObject.java
@@ -0,0 +1,37 @@
+package net.bechtelus.standard;
+
+import java.io.Serializable;
+import java.util.Properties;
+import net.bechtelus.common.ZendeskConfig;
+import org.zendesk.client.v2.Zendesk;
+
+public class APIAccessObject implements Serializable {
+
+	private static final long serialVersionUID = 7778841766245989494L;
+
+	private static Zendesk zd;
+	private static Properties config;
+
+	private APIAccessObject() {
+
+	}
+
+	public static Zendesk getAPIAccessObject() {
+
+		if (zd == null) {
+			config = ZendeskConfig.load();
+
+			Zendesk.Builder builder = new Zendesk.Builder(
+					config.getProperty("url")).setUsername(config
+					.getProperty("username"));
+			if (config.getProperty("token") != null) {
+				builder.setToken(config.getProperty("token"));
+			} else if (config.getProperty("password") != null) {
+				builder.setPassword(config.getProperty("password"));
+			}
+			zd = builder.build();
+		}
+		return zd;
+	}
+
+}
