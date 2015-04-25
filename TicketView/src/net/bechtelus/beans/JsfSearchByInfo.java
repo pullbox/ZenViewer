58a98db590cffbe222d8f019b691df22d852f27d
pullb0x <debicool98@gmail.com>
2015-04-24 23:18:25 -0400

    Refactor JsfSeachTickets to JsfSearchByInfo

--- TicketView/src/net/bechtelus/beans/JsfSearchByInfo.java
+++ TicketView/src/net/bechtelus/beans/JsfSearchByInfo.java
@@ -1,48 +1,35 @@
 package net.bechtelus.beans;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import net.bechtelus.extended.model.TicketExtended;
-import net.bechtelus.standard.APIAccessObject;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.*;
 
-import org.zendesk.client.v2.Zendesk;
-import org.zendesk.client.v2.model.Ticket;
+import org.omnifaces.util.Faces;
 
 @ManagedBean
-public class JsfSearchTickets implements Serializable {
+public class JsfSearchByInfo implements Serializable {
 
 	private static final long serialVersionUID = 7778841766245989495L;
-	private static Zendesk zd;
+
 	private String searchTerm;
-	private List<TicketExtended> tickets;
+
+	
+	
 
 	public String getSearchTerm() {
 		return searchTerm;
 	}
 
-	public List<TicketExtended> getTickets() {
-		return this.tickets;
-	}
-
+	
 	public void setSearchTerm(String searchterm) {
 		this.searchTerm = searchterm;
 	}
+	
+	
 
 	public String showSearchResults() {
 		if (!searchTerm.isEmpty()) {
-
-			tickets = null;
-			tickets = new ArrayList<TicketExtended>();
-			for (Ticket ticket : zd.getTicketsFromSearch(this.searchTerm)) {
-				// System.out.println("Body: " + comment.getBody());
-				TicketExtended ticketextended = null;
-				ticketextended = new TicketExtended(ticket);
-				tickets.add(ticketextended);
-			}
+			Faces.setFaceletAttribute(searchTerm, searchTerm);
 
 		}
 		return ("DspResult"); // Means to go to page-b.xhtml (since condition
@@ -55,7 +42,7 @@ public class JsfSearchTickets implements Serializable {
 		// mapped in faces-config.xml)
 	}
 
-	public String searchbyticketid() {
+	public String searchbyinfo() {
 		return ("SearchByInfo"); // Means to go to index.xhtml (since
 									// condition is not
 		// mapped in faces-config.xml)
@@ -63,6 +50,6 @@ public class JsfSearchTickets implements Serializable {
 
 	@PostConstruct
 	public void init() {
-		zd = APIAccessObject.getAPIAccessObject();
+
 	}
 }
